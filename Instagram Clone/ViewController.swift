4cd950e3c9c411b0b71576e8163dbe34398ba7ed
kaushik <kaushik@vajro.com>
2023-12-23 15:24:14 +0530

    Initial Commit

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by kaushik on 23/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
